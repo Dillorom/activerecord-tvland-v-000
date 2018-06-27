73bdccd153f95ade07dabb22dcdbf96ea358c19d
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-27 22:08:20 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,10 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def self.list_roles
+    binding.pry
+    self.characters
+  end
 end
